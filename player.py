6ff1973c0d5c4cc076bb3946ba934adeca08ed39
Jun720 <sekijun00@gmail.com>
2021-08-16 21:25:19 +0900

    Add seek bar

--- player.py
+++ player.py
@@ -1,5 +1,6 @@
 import vlc
 import os
+import threading
 import tkinter as tk
 import tkinter.ttk as ttk
 import tkinter.scrolledtext as st
@@ -12,10 +13,24 @@ class PyPlayer(tk.Frame):
         super().__init__(master)
 
         self.master.title("PyPlayer")
-        self.master.geometry("500x400")
+        self.master.geometry("500x500")
         self.pack()
         self.create_menu()
         self.create_palylist_panel()
+
+        # Set media seek bar
+        self.media_pos = tk.DoubleVar()
+        self.media_pos.set(0)
+        self.pos_seeker = tk.Scale(self.master,
+                                   orient='h',
+                                   resolution=0.01,
+                                   variable=self.media_pos,
+                                   command=self.set_position,
+                                   length=450,
+                                   from_=0.0,
+                                   to=1.0)
+        self.pos_seeker.pack()
+
         self.create_media_panel()
         self.create_control_panel()
 
@@ -33,18 +48,32 @@ class PyPlayer(tk.Frame):
         # Set volume bar
         self.volume = tk.DoubleVar()
         self.volume.set(100)
-        self.volume_bar = ttk.Scale(self.master,
-                                    variable=self.volume,
-                                    command=self.set_volume,
-                                    from_=0,
-                                    to=100)
+        self.volume_bar = tk.Scale(self.master,
+                                   label="Vol.",
+                                   orient='h',
+                                   variable=self.volume,
+                                   command=self.set_volume,
+                                   length=450,
+                                   from_=0,
+                                   to=100)
         self.volume_bar.pack()
+
+        self.thread1 = threading.Thread(target=self.recursive_update_seeker)
+        self.thread1.start()
+        self.thread2 = threading.Thread(target=self.recursive_update_now_playing)
+        self.thread2.start()
+
+    def ms_to_min(self, ms):
+        m, s = divmod(ms / 1000, 60)
+        h, m = divmod(m, 60)
+        return str(round(h)).zfill(2) + ":" + str(round(m)).zfill(2) + ":" + str(round(s)).zfill(2)
+
 
     def create_menu(self):
         self.menu_bar = tk.Menu(self.master)
         self.master.config(menu=self.menu_bar)
 
-        self.menu_file = tk.Menu(self.menu_bar,  tearoff=0)
+        self.menu_file = tk.Menu(self.menu_bar, tearoff=0)
         self.menu_bar.add_cascade(label="File", menu=self.menu_file)
         self.menu_file.add_command(label="Open", command=self.open_file)
         self.menu_file.add_command(label="Clear One", command=self.clear_one)
@@ -79,8 +108,6 @@ class PyPlayer(tk.Frame):
         files = askopenfilenames(filetypes=self.type, initialdir=self.init_dir)
         if files != "":
             for file in files:
-                print(file)
-                print(type(file))
                 media = vlc.Media(file)
                 self.media_list.add_media(media)
                 self.player.set_media_list(self.media_list)
@@ -103,7 +130,6 @@ class PyPlayer(tk.Frame):
     def drop(self, event):
         if event.data:
             files = event.data.strip('{}').split('} {')
-            print(files)
             for file in files:
                 media = vlc.Media(file)
                 self.media_list.add_media(media)
@@ -145,14 +171,24 @@ class PyPlayer(tk.Frame):
         if self.play_button['text'] == "Play":
             self.play_button['text'] = "Pause"
 
+    def set_position(self, event=None):
+        self.player.get_media_player().set_position(float(self.media_pos.get()))
+
+    def set_volume(self, event=None):
+        self.player.get_media_player().audio_set_volume(int(self.volume.get()))
+
     def get_now_playing(self):
         if self.player.get_media_player().get_media() is not None:
             return self.player.get_media_player().get_media()
 
     def update_now_playing(self):
         playing = self.get_now_playing()
-        if playing is not None:
+        if playing is not None and self.now_playing.cget("text") != playing.get_meta(vlc.Meta.Title):
             self.now_playing['text'] = playing.get_meta(vlc.Meta.Title)
+            idx = self.media_list.index_of_item(playing)
+            for i in range(self.media_list.count()):
+                self.playlist_log.tag_config(i, foreground="black")
+            self.playlist_log.tag_config(str(idx), foreground="blue")
 
     def update_playlist_log(self):
         self.playlist_log.configure(state='normal')
@@ -162,8 +198,15 @@ class PyPlayer(tk.Frame):
             self.playlist_log.insert('end', log + "\n", i)
         self.playlist_log.configure(state='disable')
 
-    def set_volume(self, event=None):
-        self.player.get_media_player().audio_set_volume(int(self.volume.get()))
+    def recursive_update_seeker(self):
+        pos = self.player.get_media_player().get_position()
+        self.media_pos.set(pos)
+        self.pos_seeker['label'] = self.ms_to_min(self.player.get_media_player().get_time())
+        self.after(1000, self.recursive_update_seeker)
+
+    def recursive_update_now_playing(self):
+        self.update_now_playing()
+        self.after(10000, self.recursive_update_now_playing)
 
 
 if __name__ == '__main__':
